55c34530353e019a5e96ad2f4962e13290e5f59d
dio <2634716776@qq.com>
2025-12-07 15:55:18 +0800

    feat(mainWidget): 设计单例模式 设计单例模式

--- ChatClient/mainwidget.h
+++ ChatClient/mainwidget.h
@@ -3,12 +3,27 @@
 
 #include <QWidget>
 
+/**
+ * @brief The MainWidget class
+ * @details
+ * 设计为单例类(懒汉加载模式)
+ */
 class MainWidget : public QWidget
 {
     Q_OBJECT
 
 public:
-    MainWidget(QWidget *parent = nullptr);
     ~MainWidget();
+
+public:
+    static MainWidget* getInstance();
+
+private:
+    // 单例实例
+    static MainWidget* instance;
+
+    // 单例需要将构造私有化
+    MainWidget(QWidget *parent = nullptr);
+
 };
 #endif // MAINWIDGET_H
