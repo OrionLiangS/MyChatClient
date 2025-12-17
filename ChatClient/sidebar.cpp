f44e2f140d129988c4189345d7811e34b9236ecf
dio <2634716776@qq.com>
2025-12-17 21:04:38 +0800

    feat(sidebar): 新增了抽屉类 1. 新增抽屉类并对抽屉类进行了实现 2. 修复了messageshowarea中的ResizeEvent中可能出现的栈溢出 3. 连接了RightWindow的titleExtraBtn与sidebar的信号槽连接 4. 增加了一点注释

---
+++ ChatClient/sidebar.cpp
@@ -0,0 +1,143 @@
+#include "sidebar.h"
+
+#include <QEvent>
+
+#include <QMouseEvent>
+
+#include <QStyleOption>
+
+#include <QPainter>
+
+#include <QStyle>
+
+
+Sidebar::Sidebar(QWidget *parent)
+    :QWidget(parent), m_parent(parent), m_content(nullptr), m_isShow(false)
+{
+    this->setObjectName("Sidebar");
+
+    // ============================
+    // 1) 安装事件监听器
+    // ============================
+    // 安装事件监听器需要保证父对象必须存在 否则无法安装时间监听器
+    if(m_parent) {
+        this->setParent(parent);
+        // 安装事件监听器 用于监听父类的事件
+        m_parent->installEventFilter(this);
+    }
+
+    // ===========================
+    // 初始化抽屉
+    // ===========================
+    // 设置大小
+    this->setFixedSize(m_width, m_parent->height()); //
+    this->hide();
+
+    // 初始化布局
+    m_layout = new QVBoxLayout(this);
+    m_layout->setContentsMargins(0,0,0,0);
+
+    // 初始化动画
+    m_anim = new QPropertyAnimation(this, "pos");
+    // 持续事件
+    m_anim->setDuration(300);
+    // 设置丝滑曲线 todo
+    m_anim->setEasingCurve(QEasingCurve::OutCubic);
+
+}
+
+
+
+void Sidebar::setContent(QWidget *content)
+{
+    // 判断 m_content 是否存在内容, 若是存在则清理原有的内容, 将
+    if(m_content){
+        m_layout->removeWidget(m_content);
+        m_content->deleteLater();
+    }
+    m_content = content;
+    // 将新内容加进Layout布局中
+    m_layout->addWidget(m_content);
+}
+
+void Sidebar::showSidebar()
+{
+    // 断开上次连接
+    disconnect(m_anim, &QPropertyAnimation::finished, this, &Sidebar::hide);
+
+    // 显示前先更新大小
+    updatePosition();
+    this->show();
+    this->raise(); // 顶层显式
+
+    // 设置动画进入
+    int parentW = m_parent->width();
+    m_anim->stop();
+    m_anim->setStartValue(QPoint(parentW, titleHeight));
+    m_anim->setEndValue(QPoint(parentW-m_width, titleHeight));
+    m_anim->start();
+
+    m_isShow = true;
+}
+
+void Sidebar::hideSidebar()
+{
+    int parentW = m_parent->width(); // 获取父类长度
+    m_anim->stop();
+    m_anim->setStartValue(this->pos());
+    m_anim->setEndValue(QPoint(parentW, titleHeight));
+
+    connect(m_anim, &QPropertyAnimation::finished, this, &Sidebar::hide, Qt::UniqueConnection);
+
+    m_anim->start();
+    m_isShow = false;
+}
+
+void Sidebar::toggle()
+{
+    if(m_isShow) hideSidebar();
+    else showSidebar();
+}
+
+bool Sidebar::eventFilter(QObject *watched, QEvent *event)
+{
+    // 监听 ResizeEvent 事件
+    if(watched == m_parent && event->type() == QEvent::Resize){
+        // 父窗口大小发生变化, update更新大小
+        updatePosition();
+    }
+    if(m_isShow && event->type() == QEvent::MouseButtonPress){
+        QMouseEvent *mouseEvent = static_cast<QMouseEvent*>(event);
+        // mapToGlobal 确保坐标系一致
+        QPoint globalPos = mouseEvent->globalPos();
+
+        // 如果点击点 不在 Sidebar 的矩形范围内
+        // 且 Sidebar 是显示的
+        if (!this->geometry().contains(this->mapFromGlobal(globalPos))) {
+            hideSidebar();
+            return true; // 拦截事件，不让父窗口响应这次点击 (可选)
+        }
+    }
+    return QWidget::eventFilter(watched, event);
+}
+
+void Sidebar::paintEvent(QPaintEvent *event)
+{
+    QStyleOption opt;
+    opt.initFrom(this);
+    QPainter p(this);
+    style()->drawPrimitive(QStyle::PE_Widget, &opt, &p, this);
+}
+
+void Sidebar::updatePosition()
+{
+    if(!m_parent) return; // 无父亲
+    this->setFixedSize(m_width, m_parent->height());
+
+    if(!m_isShow){
+        this->move(m_parent->width(), titleHeight); // 移到界外
+    }
+    else this->move(m_parent->width()-m_width, titleHeight); // 吸附右侧
+}
+
+
---
+++ ChatClient/sidebar.h
@@ -0,0 +1,93 @@
+#ifndef SIDEBAR_H
+#define SIDEBAR_H
+
+#include <QObject>
+
+#include <QWidget>
+
+#include <QVBoxLayout>
+
+#include <QPropertyAnimation> ///< 用于绘制动画
+
+
+/**
+ * @brief The Sidebar class
+ * @details
+ * 抽屉容器 - 暂时用于作右上角按钮的会话详情页
+ */
+class Sidebar : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit Sidebar(QWidget *parent = nullptr);
+
+    /**
+     * @brief setContent 为抽屉容器塞进内容
+     * @param content
+     */
+    void setContent(QWidget* content);
+
+    /**
+     * @brief showSidebar
+     * @details
+     * 展示抽屉
+     */
+    void showSidebar();
+
+    /**
+     * @brief hideSidebar
+     * @details
+     * 隐藏抽屉
+     */
+    void hideSidebar();
+
+    /**
+     * @brief toggle
+     * @details
+     * 自动切换抽屉状态
+     */
+    void toggle();
+
+protected:
+    /**
+     * @brief eventFilter
+     * @param watched
+     * @param event
+     * @return
+     * @details
+     * 拦截父窗口的事件, 来判断当前是需要隐藏
+     */
+    bool eventFilter(QObject *watched, QEvent *event) override;
+
+    /**
+     * @brief paintEvent
+     * @param event
+     * @details
+     * 绘制窗口 - 当状态为show时需要进行展示
+     */
+    void paintEvent(QPaintEvent *event)override;
+
+
+private:
+    // 默认情况下传入RightWindow
+    QWidget *m_parent;              ///< 记录父窗口
+    QWidget *m_content;             ///< 内部内容
+    QVBoxLayout *m_layout;          ///< 内部布局
+    QPropertyAnimation *m_anim;     ///< 动画对象
+    bool m_isShow;                  ///< 当前状态
+    int m_width =255;               ///< 侧边栏宽度
+
+    /**
+     * @brief updatePosition
+     * @details
+     * 根据父窗口的大小自适应大小
+     */
+    void updatePosition();
+
+
+    const int titleHeight = 67;     ///< 硬编码 Title高度
+
+signals:
+};
+
+#endif // SIDEBAR_H
